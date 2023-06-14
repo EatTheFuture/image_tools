2a754b1b5f8e567e8e53d6b03e75044b4c006543
Nathan Vegdahl <cessen@cessen.com>
2023-06-14 18:23:01 +0200

    Tonemapper: tweak luma weights for better red rolloff.

--- sub_crates/ocio_gen/src/tone_map.rs
+++ sub_crates/ocio_gen/src/tone_map.rs
@@ -117,7 +117,7 @@ impl Tonemapper {
         // to pass its own luma function, so that it can be specific to the
         // given color space and as sophisticated as needed.
         fn luma(rgb: [f64; 3]) -> f64 {
-            const LUMA_WEIGHTS: [f64; 3] = [2.0, 10.0, 1.0];
+            const LUMA_WEIGHTS: [f64; 3] = [1.0, 10.0, 1.0];
             ((rgb[0] * LUMA_WEIGHTS[0]) + (rgb[1] * LUMA_WEIGHTS[1]) + (rgb[2] * LUMA_WEIGHTS[2]))
                 / LUMA_WEIGHTS.iter().sum::<f64>()
         }
