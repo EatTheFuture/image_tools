532a3677ef73b2604fb0700d358590569dd718c4
Nathan Vegdahl <cessen@cessen.com>
2023-09-14 00:04:00 +0200

    Refactor AgX code.
    In preparation for building the LUT generating and OCIO transform
    generating code.

--- sub_crates/ocio_gen/src/agx.rs
+++ sub_crates/ocio_gen/src/agx.rs
@@ -3,141 +3,209 @@
 //! Aims to match Blender's AgX implementation.
 
 use colorbox::{
-    chroma, matrix,
+    chroma::{self, Chromaticities},
+    matrix::{self, Matrix},
     transforms::{
         ocio::{hsv_to_rgb, rgb_to_hsv},
         rgb_gamut,
     },
 };
 
-fn agx_base_rec2020(col: [f64; 3], mix_percent: f64) -> [f64; 3] {
-    fn apply(xyz: [f64; 3], f: impl Fn(f64) -> f64) -> [f64; 3] {
-        [f(xyz[0]), f(xyz[1]), f(xyz[2])]
-    }
-
-    fn dot(a: [f64; 3], b: [f64; 3]) -> f64 {
-        (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2])
-    }
-
-    // Curve parameters.
-    const MIDGREY: f64 = 0.18;
+pub fn agx_base_rec2020() -> AgX {
+    const MID_GRAY: f64 = 0.18;
     const NORMALIZED_LOG2_MINIMUM: f64 = -10.0;
     const NORMALIZED_LOG2_MAXIMUM: f64 = 6.5;
-    let x_pivot =
-        NORMALIZED_LOG2_MINIMUM.abs() / (NORMALIZED_LOG2_MAXIMUM - NORMALIZED_LOG2_MINIMUM);
-    let y_pivot = MIDGREY.powf(1.0 / 2.4);
-    let exponent = [1.5, 1.5];
-    let slope = 2.4;
 
-    // Gamut conversion prep.
-    let luma_coeffs = [0.2658180370250449, 0.59846986045365, 0.1357121025213052];
-    let inset_matrix = matrix::rgb_to_rgb_matrix(
-        space::create_working_space([3.0, -1.0, -1.0], [0.4, 0.22, 0.13], chroma::REC709),
-        chroma::REC709,
-    );
-    let outset_matrix = matrix::rgb_to_rgb_matrix(
-        chroma::REC709,
-        space::create_working_space([0.0, 0.0, 0.0], [0.4, 0.22, 0.04], chroma::REC709),
-    );
+    // Sigmoid definition.
+    let sigmoid = {
+        let x_pivot =
+            NORMALIZED_LOG2_MINIMUM.abs() / (NORMALIZED_LOG2_MAXIMUM - NORMALIZED_LOG2_MINIMUM);
+        let y_pivot = MID_GRAY.powf(1.0 / 2.4);
 
-    //--------
-
-    // Apply open-domain gamut clip.
-    let col = rgb_gamut::open_domain_clip(col, dot(col, luma_coeffs).max(0.0), 0.0);
-
-    // Apply inset matrix.
-    let col = matrix::transform_color(col, inset_matrix);
-
-    // Record current chromaticity angle.
-    let pre_form_hsv = rgb_to_hsv(col);
-
-    // Apply Log2 curve to prepare for sigmoid.
-    let log = apply(col, |x| {
-        log2_encoding(x, MIDGREY, NORMALIZED_LOG2_MINIMUM, NORMALIZED_LOG2_MAXIMUM)
-    });
-
-    // Apply sigmoid.
-    let col = apply(col, |x| {
-        sigmoid::sigmoid(
-            x,
+        curve::Sigmoid::new(
             [x_pivot, y_pivot],
-            slope,
-            [0.0, 0.0],
-            exponent,
-            [[0.0, 0.0], [1.0, 1.0]],
+            2.4,
+            [0.0; 2],
+            [1.5; 2],
+            [[0.0; 2], [1.0; 2]],
         )
-    });
-
-    // Linearize
-    let col = apply(col, |x| x.powf(2.4));
-
-    // Record post-sigmoid chroma angle.
-    let col = rgb_to_hsv(col);
-
-    // Mix pre-formation chroma angle with post formation chroma angle.
-    let hue = {
-        // This looks involved, but is ultimately just a lerp between two
-        // hue angles.  The complication is that since hue loops back on
-        // itself, we need to ensure that we're interpolating on the
-        // shortest path between the two hue angles.
-        let h1 = pre_form_hsv[0];
-        let mut h2 = col[0];
-        while (h1 - h2) > 0.5 {
-            h2 += 1.0;
-        }
-        while (h1 - h2) < 0.5 {
-            h2 -= 1.0;
-        }
-        let t = mix_percent / 100.0;
-        let mut h3 = (h1 * (1.0 - t)) + (h2 * t);
-        while h3 < 0.0 {
-            h3 += 1.0;
-        }
-        while h3 > 1.0 {
-            h3 -= 1.0;
-        }
-        h3
     };
 
-    let col = hsv_to_rgb([hue, col[1], col[2]]);
-
-    // Apply outset to make the result more chroma-laden.
-    let col = matrix::transform_color(col, outset_matrix);
-
-    return col;
+    AgX::new(
+        chroma::REC709,
+        [3.0, -1.0, -1.0],
+        [0.4, 0.22, 0.13],
+        [0.0, 0.0, 0.0],
+        [0.4, 0.22, 0.04],
+        MID_GRAY,
+        [NORMALIZED_LOG2_MINIMUM, NORMALIZED_LOG2_MAXIMUM],
+        sigmoid,
+        [0.2658180370250449, 0.59846986045365, 0.1357121025213052],
+        40.0,
+    )
 }
 
-fn log2_encoding(lin: f64, middle_grey: f64, min_exposure: f64, max_exposure: f64) -> f64 {
-    let lg2 = (lin / middle_grey).log2();
-    (lg2 - min_exposure) / (max_exposure - min_exposure)
+#[derive(Debug, Copy, Clone)]
+pub struct AgX {
+    log_range: [f64; 2],
+    mid_gray: f64,
+
+    inset_matrix: Matrix,
+    outset_matrix: Matrix,
+    luminance_coeffs: [f64; 3],
+
+    sigmoid: curve::Sigmoid,
+
+    mix_percent: f64,
 }
 
-fn log2_decoding(log_norm: f64, middle_grey: f64, min_exposure: f64, max_exposure: f64) -> f64 {
-    let lg2 = log_norm * (max_exposure - min_exposure) + min_exposure;
-    2.0_f64.powf(lg2) * middle_grey
+impl AgX {
+    pub fn new(
+        color_space: Chromaticities,
+        inset_rotations: [f64; 3],
+        inset_insets: [f64; 3],
+        outset_rotations: [f64; 3],
+        outset_insets: [f64; 3],
+        mid_gray: f64,
+        log_range: [f64; 2],
+        sigmoid: curve::Sigmoid,
+        luminance_coeffs: [f64; 3],
+        mix_percent: f64,
+    ) -> Self {
+        let inset_matrix = matrix::rgb_to_rgb_matrix(
+            space::create_working_space(inset_rotations, inset_insets, color_space),
+            color_space,
+        );
+        let outset_matrix = matrix::rgb_to_rgb_matrix(
+            color_space,
+            space::create_working_space(outset_rotations, outset_insets, color_space),
+        );
+
+        Self {
+            log_range: log_range,
+            mid_gray: mid_gray,
+            inset_matrix: inset_matrix,
+            outset_matrix: outset_matrix,
+            luminance_coeffs: luminance_coeffs,
+            sigmoid: sigmoid,
+            mix_percent: mix_percent,
+        }
+    }
+
+    pub fn eval_1d(&self, x: f64) -> f64 {
+        let x = curve::log2_encoding(x, self.mid_gray, self.log_range[0], self.log_range[1]);
+        let x = self.sigmoid.eval(x);
+        x.powf(2.4)
+    }
+
+    pub fn eval(&self, col: [f64; 3]) -> [f64; 3] {
+        fn dot(a: [f64; 3], b: [f64; 3]) -> f64 {
+            (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2])
+        }
+
+        // Apply open-domain gamut clip.
+        // Note: in the original from Eary, this was instead the "lower guard
+        // rail" application.  I've substituted a simple luminance-based
+        // gamut clip, as I believe that's essentially what "lower guard
+        // rail" is, just with an unnecessarily complex implementation.
+        let col = rgb_gamut::open_domain_clip(col, dot(col, self.luminance_coeffs).max(0.0), 0.0);
+
+        let col = matrix::transform_color(col, self.inset_matrix);
+
+        let pre_form_hsv = rgb_to_hsv(col);
+
+        let col = [
+            self.eval_1d(col[0]),
+            self.eval_1d(col[1]),
+            self.eval_1d(col[2]),
+        ];
+
+        // Record post-sigmoid chroma angle.
+        let col = rgb_to_hsv(col);
+
+        // Mix pre-formation chroma angle with post formation chroma angle.
+        let hue = {
+            // This looks involved, but is ultimately just a lerp between two
+            // hue angles.  The complication is that since hue loops back on
+            // itself, we need to ensure that we're interpolating on the
+            // shortest path between the two hue angles.
+            let h1 = pre_form_hsv[0];
+            let mut h2 = col[0];
+            while (h1 - h2) > 0.5 {
+                h2 += 1.0;
+            }
+            while (h1 - h2) < 0.5 {
+                h2 -= 1.0;
+            }
+            let t = self.mix_percent / 100.0;
+            let mut h3 = (h1 * (1.0 - t)) + (h2 * t);
+            while h3 < 0.0 {
+                h3 += 1.0;
+            }
+            while h3 > 1.0 {
+                h3 -= 1.0;
+            }
+            h3
+        };
+
+        let col = hsv_to_rgb([hue, col[1], col[2]]);
+
+        // Apply outset to make the result more chroma-laden.
+        let col = matrix::transform_color(col, self.outset_matrix);
+
+        // Do a final closed-domain clip to ensure all colors are in-gamut.
+        let luma = dot(col, self.luminance_coeffs).max(0.0);
+        rgb_gamut::closed_domain_clip(rgb_gamut::open_domain_clip(col, luma, 0.0), luma, 0.0)
+    }
 }
 
-mod sigmoid {
-    /// A sigmoi
-    pub fn sigmoid(
-        x: f64,
-        // Pivot coordinates x and y for the fulcrum.
-        pivots: [f64; 2],
-        // Slope of linear portion.
-        slope: f64,
-        // Length of transition toward the toe and shoulder.
-        lengths: [f64; 2],
-        // Exponential power of the toe and shoulder regions.
-        powers: [f64; 2],
-        // Intersection limit coordinates x and y for the toe and shoulder.
-        limits: [[f64; 2]; 2],
+mod curve {
+    pub fn log2_encoding(lin: f64, middle_grey: f64, min_exposure: f64, max_exposure: f64) -> f64 {
+        let lg2 = (lin / middle_grey).log2();
+        (lg2 - min_exposure) / (max_exposure - min_exposure)
+    }
+
+    pub fn log2_decoding(
+        log_norm: f64,
+        middle_grey: f64,
+        min_exposure: f64,
+        max_exposure: f64,
     ) -> f64 {
-        let transition_toe_x = linear_breakpoint(-lengths[0], slope, pivots[0]);
-        let transition_toe_y = linear_breakpoint(slope * -lengths[0], slope, pivots[1]);
-        let transition_shoulder_x = linear_breakpoint(lengths[1], slope, pivots[0]);
-        let transition_shoulder_y = linear_breakpoint(slope * lengths[1], slope, pivots[1]);
+        let lg2 = log_norm * (max_exposure - min_exposure) + min_exposure;
+        2.0_f64.powf(lg2) * middle_grey
+    }
 
-        if x < transition_toe_x {
+    #[derive(Debug, Copy, Clone)]
+    pub struct Sigmoid {
+        // Toe.
+        transition_toe: [f64; 2],
+        scale_toe: f64,
+        power_toe: f64,
+        // Linear segment.
+        slope: f64,
+        intercept: f64,
+        // Shoulder.
+        transition_shoulder: [f64; 2],
+        scale_shoulder: f64,
+        power_shoulder: f64,
+    }
+
+    impl Sigmoid {
+        pub fn new(
+            // Pivot coordinates x and y for the fulcrum.
+            pivots: [f64; 2],
+            // Slope of linear portion.
+            slope: f64,
+            // Length of transition toward the toe and shoulder.
+            lengths: [f64; 2],
+            // Exponential power of the toe and shoulder regions.
+            powers: [f64; 2],
+            // Intersection limit coordinates x and y for the toe and shoulder.
+            limits: [[f64; 2]; 2],
+        ) -> Self {
+            // Toe.
+            let transition_toe_x = linear_breakpoint(-lengths[0], slope, pivots[0]);
+            let transition_toe_y = linear_breakpoint(slope * -lengths[0], slope, pivots[1]);
             let inverse_transition_toe_x = 1.0 - transition_toe_x;
             let inverse_transition_toe_y = 1.0 - transition_toe_y;
             let inverse_limit_toe_x = 1.0 - limits[0][0];
@@ -150,18 +218,13 @@ mod sigmoid {
                 powers[0],
                 slope,
             );
-            exponential_curve(
-                x,
-                scale_toe,
-                slope,
-                powers[0],
-                transition_toe_x,
-                transition_toe_y,
-            )
-        } else if x <= transition_shoulder_x {
+
+            // Linear segment.
             let intercept = transition_toe_y - (slope * transition_toe_x);
-            line(x, slope, intercept)
-        } else {
+
+            // Shoulder.
+            let transition_shoulder_x = linear_breakpoint(lengths[1], slope, pivots[0]);
+            let transition_shoulder_y = linear_breakpoint(slope * lengths[1], slope, pivots[1]);
             let scale_shoulder = scale(
                 limits[1][0],
                 limits[1][1],
@@ -170,14 +233,44 @@ mod sigmoid {
                 powers[1],
                 slope,
             );
-            exponential_curve(
-                x,
-                scale_shoulder,
-                slope,
-                powers[1],
-                transition_shoulder_x,
-                transition_shoulder_y,
-            )
+
+            Self {
+                // Toe.
+                transition_toe: [transition_toe_x, transition_toe_y],
+                scale_toe: scale_toe,
+                power_toe: powers[0],
+                // Linear segment.
+                slope: slope,
+                intercept: intercept,
+                // Shoulder.
+                transition_shoulder: [transition_shoulder_x, transition_shoulder_y],
+                scale_shoulder: scale_shoulder,
+                power_shoulder: powers[1],
+            }
+        }
+
+        pub fn eval(&self, x: f64) -> f64 {
+            if x < self.transition_toe[0] {
+                exponential_curve(
+                    x,
+                    self.scale_toe,
+                    self.slope,
+                    self.power_toe,
+                    self.transition_toe[0],
+                    self.transition_toe[1],
+                )
+            } else if x <= self.transition_shoulder[0] {
+                line(x, self.slope, self.intercept)
+            } else {
+                exponential_curve(
+                    x,
+                    self.scale_shoulder,
+                    self.slope,
+                    self.power_shoulder,
+                    self.transition_shoulder[0],
+                    self.transition_shoulder[1],
+                )
+            }
         }
     }
 
@@ -230,7 +323,7 @@ mod space {
     ///
     /// Adapted from:
     /// https://github.com/sobotka/SB2383-Configuration-Generation/blob/e507709c4dc0/working_space.py
-    /// by Troy Sobotka
+    /// by Troy Sobotka.
     pub fn create_working_space(
         primaries_rotate: [f64; 3],
         primaries_inset: [f64; 3],
@@ -345,16 +438,10 @@ mod space {
         (tmp, slope_a * tmp + offset_a)
     }
 
-    fn dist2(a: (f64, f64), b: (f64, f64)) -> f64 {
-        let x = a.0 - b.0;
-        let y = a.1 - b.1;
-        (x * x) + (y * y)
-    }
-
     #[cfg(test)]
     mod tests {
         use super::*;
-        use colorbox::{chroma, matrix};
+        use colorbox::chroma;
 
         fn is_close(a: f64, b: f64, thresh: f64) -> bool {
             (a - b).abs() < thresh
